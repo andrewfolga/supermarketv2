8748df7bdd50233f3e1a7b16622627a5c215b83f
Andrew Folga <andrew.folga@digital.hmrc.gov.uk>
2017-05-02 17:25:29 +0100

    Add Receipt.

---
+++ src/main/java/qmetric/supermarket/domain/Receipt.java
@@ -0,0 +1,25 @@
+package qmetric.supermarket.domain;
+
+import java.math.BigDecimal;
+import java.util.Map;
+
+/**
+ * Created by andrzejfolga on 01/05/2017.
+ */
+public class Receipt {
+
+    private final Map<String, BigDecimal> itemsPrices;
+    private final Map<String, BigDecimal> savings;
+    private final BigDecimal subTotal;
+    private final BigDecimal totalSavings;
+    private final BigDecimal totalToPay;
+
+    public Receipt(Map<String, BigDecimal> itemsPrices, Map<String, BigDecimal> savings) {
+        this.itemsPrices = itemsPrices;
+        this.savings = savings;
+        this.subTotal = itemsPrices.values().stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        this.totalSavings = savings.values().stream().reduce(BigDecimal.ZERO, BigDecimal::add);
+        this.totalToPay = subTotal.add(totalSavings);
+    }
+
+}

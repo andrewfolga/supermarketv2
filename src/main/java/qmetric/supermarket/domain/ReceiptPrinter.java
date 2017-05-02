cc30155ff6bf77104a5f215e85e098ee6c944051
Andrew Folga <andrew.folga@digital.hmrc.gov.uk>
2017-05-02 20:59:08 +0100

    Add Receipt and ReceiptPrinter.

--- src/main/java/qmetric/supermarket/domain/ReceiptPrinter.java
+++ src/main/java/qmetric/supermarket/domain/ReceiptPrinter.java
@@ -1,12 +1,19 @@
 package qmetric.supermarket.domain;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
  * Created by andrzejfolga on 02/05/2017.
  */
 public class ReceiptPrinter {
 
+    public static final String TEMPLATE = "%1$-20s%2$5s";
+
     public String print(Receipt receipt) {
-        return "";
+        List<String> printableReceiptLines = receipt.apply((String k, String v) -> String.format(TEMPLATE, k, v));
+        String printableReceipt = printableReceiptLines.stream().collect(Collectors.joining("\n"));
+        return printableReceipt;
     }
 
 }

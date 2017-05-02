550e0d485e19c48eef038a3da80b5036c6b92367
Andrew Folga <andrew.folga@digital.hmrc.gov.uk>
2017-05-02 21:09:32 +0100

    Create ReceiptBuilder.

---
+++ src/test/java/qmetric/domain/ReceiptBuilderTest.java
@@ -0,0 +1,21 @@
+package qmetric.domain;
+
+import org.junit.Test;
+import qmetric.supermarket.domain.ReceiptBuilder;
+
+/**
+ * Created by andrzejfolga on 01/05/2017.
+ */
+public class ReceiptBuilderTest {
+
+    private ReceiptBuilder receiptBuilder = new ReceiptBuilder();;
+
+    @Test
+    public void shouldBuildBasicReceipt() throws Exception {
+    }
+
+    @Test
+    public void shouldBuildReceiptWithPromotions() throws Exception {
+    }
+
+}
